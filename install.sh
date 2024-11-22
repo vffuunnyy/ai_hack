d2246df4d0248562fab0e884f3979b5564b4232c
VitaliiSmirnov <temmets20@gmail.com>
2024-11-22 13:51:10 +0700

    new conda instruction

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
-uv venv --python 3.12
-uv pip install torch
-uv sync --no-build-isolation
+# uv venv --python 3.12
+# uv pip install torch
+# uv sync --no-build-isolation
+
+conda create -n ai-hack python=3.12
+conda activate ai-hack
+
+conda install numpy pandas scikit-learn pytorch torchvision -c defaults -c pytorch -c conda-forge
+pip install torch-geometric torch-cluster torch-scatter mesh-reducer
+
